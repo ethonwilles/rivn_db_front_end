ecfb220dbf727f3d49252355469513abe2812d86
ethonwilles <ethonwilles@gmail.com>
2020-02-19 15:56:17 -0700

    adfs

--- src/components/listAllVendors.js
+++ src/components/listAllVendors.js
@@ -15,7 +15,7 @@ const ListAll = () => {
   const renderVendors = () => {
     if (allVendors) {
       return allVendors.map(elem => {
-        if(elem.display == "y" || elem.vendor_id == 'ethon'){
+        if(elem.active == "y" || elem.vendor_id == 'ethon'){
         return (
           <Card
             name={elem.name}
